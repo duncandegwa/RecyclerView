2cfbd02781230a0970c991d4fe21d8a14f3c3057
duncandegwa <74187010+duncandegwa@users.noreply.github.com>
2022-03-22 20:38:37 +0300

    removed Django

--- app/src/main/java/com/droiderscage/recyclerview/data/PersonRepository.kt
+++ app/src/main/java/com/droiderscage/recyclerview/data/PersonRepository.kt
@@ -1,14 +1,21 @@
 package com.droiderscage.recyclerview.data
-
-import com.droiderscage.recyclerview.data.Person
+ import com.droiderscage.recyclerview.data.Person
 
 class PersonRepository {
     // This should contain ROOM methods or remote API fun that deals with saving and retrieving data
     // break the above strategy and ignore the image param below for a while...
     fun getPersonData(): ArrayList<Person> = arrayListOf(
-        Person("Eric", "gacoki@gmail.com", 0),
-        Person("Paul", "pauldroider@gmail.com",0),
-        Person("John Doe", "doejohn@gmail.com", 0),
-        Person("Sudo", "sudos@gmail.com", 0),
+        Person("Felix", "felix45@gmail.com",0),
+        Person("Jeniffer", "jen@gmail.com", 0),
+        Person("Simon", "symons@gmail.com", 0),
+        Person("Felix", "felix45@gmail.com",0),
+        Person("Jeniffer", "jen@gmail.com", 0),
+        Person("Simon", "symons@gmail.com", 0),
+        Person("Felix", "felix45@gmail.com",0),
+        Person("Jeniffer", "jen@gmail.com", 0),
+        Person("Simon", "symons@gmail.com", 0),
+        Person("Felix", "felix45@gmail.com",0),
+        Person("Jeniffer", "jen@gmail.com", 0),
+        Person("Simon", "symons@gmail.com", 0),
     )
 }
